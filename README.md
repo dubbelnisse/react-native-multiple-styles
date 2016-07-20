8570406fad3f1d49d6f382de3ac0a581e55d3652
Nils Nilsson <nilspontusnilsson@gmail.com>
2016-07-20 12:56:05 +0200

    Fixed readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # react-native-multiple-styles
+
+A simple utility for handeling mutiple styles (classes) in react-native
+
+Install with npm.
+
+```sh
+npm install react-native-multiple-styles --save
+```
+
+Import it to your project
+```js
+import multipleStyles from 'react-native-multiple-styles'
+```
+
+## Usage
+```js
+var styles = StyleSheet.create({
+  icon: {
+    backgroundColor: 'black'
+  },
+  iconRed: {
+    backgroundColor: 'red'
+  }
+});
+
+multipleStyles(styles.icon, styles.iconRed) // => [1, 2]
+multipleStyles(styles.icon, { [styles.iconRed]: true }) // => [1, 2]
+multipleStyles(styles.icon, { [styles.iconRed]: false }) // => [1]
+```
+
+It returns an array of the IDs representing the style objects. More info about that [here](https://facebook.github.io/react-native/docs/stylesheet.html).
--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-var _ = require('lodash')
-
 module.exports = function() {
   var hasOwn = {}.hasOwnProperty;
   var styles = []
 
-  if(arguments.length === 0) {
-    styles.push(arguments[0])
-  } else {
-    for (var i = 0; i < arguments.length; i++) {
-      var arg = arguments[i]
-			if (!arg) continue
-
-			var argType = typeof arg
-
-			if (argType === 'object') {
-				for (var key in arg) {
-					if (hasOwn.call(arg, key) && arg[key] === true) {
-						styles.push(key)
-					} else if (arg[key] !== false) {
-            styles.push(arg)
-          }
+  for (var key in arguments) {
+    if (typeof arguments[key] === 'object') {
+      var arg = arguments[key]
+      for (var key in arg) {
+				if (hasOwn.call(arg, key) && arg[key] === true) {
+					styles.push(parseInt(key))
 				}
 			}
+    } else {
+      styles.push(arguments[key])
     }
   }
 
