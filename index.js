96d3f282243c6553ed827fc80ec58f363f56cf14
Nils Nilsson <nilspontusnilsson@gmail.com>
2016-07-20 11:05:47 +0200

    Added basic functionallity for condisioning styles

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import _ from 'lodash'
+var _ = require('lodash')
 
-exports function () {
-  console.log("This is reqact-native-classnames");
+module.exports = function() {
+  var hasOwn = {}.hasOwnProperty;
+  var styles = []
+
+  if(arguments.length === 0) {
+    styles.push(arguments[0])
+  } else {
+    for (var i = 0; i < arguments.length; i++) {
+      var arg = arguments[i]
+			if (!arg) continue
+
+			var argType = typeof arg
+
+			if (argType === 'object') {
+				for (var key in arg) {
+					if (hasOwn.call(arg, key) && arg[key] === true) {
+						styles.push(key)
+					} else if (arg[key] !== false) {
+            styles.push(arg)
+          }
+				}
+			}
+    }
+  }
+
+  return styles
 }
